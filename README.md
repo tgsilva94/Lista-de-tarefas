4ee1a8cab1ca176ab845d9be582a8825e46c1ab1
tiago <tgsilva.fx@hotnail.com>
2022-01-23 22:21:48 -0300

    primeiro commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Lista-de-tarefas
+Este projeto foi criado com o intuito de colocar em prática meus os conhecimentos adquiridos em JavaScript. A ideia inicial é desenvolver um Todo List ir acrescentando recursos à medida que avanço com meus estudos.
+
+# Tecnologias-ultilizadas
+- HTML/CSS/JAVASCRIPT
+
+# O-Projeto
+![gif do projeto](gifApp.gif)
---
+++ app.js
@@ -0,0 +1,60 @@
+'use strict';
+{/* <label class="todo__item">
+                <input type="checkbox">
+                <div>teste de item 1</div>
+                <input type="button" value="X">
+            </label> */}
+let banco = [
+    { 'tarefa': 'Estudar Js', 'Estudar ingles': '' },
+    { 'tarefa': 'Estudar html', 'Estudar css': '' }
+]
+const criarItem = (tarefa, status, indice) => {
+    const item = document.createElement('label');
+    item.classList.add('todo__item');
+    item.innerHTML = `
+    <input type="checkbox" ${status} data-indice=${indice}>
+    <div>${tarefa}</div>
+    <input type="button" value="X"  data-indice=${indice}>
+    `
+    document.getElementById('todoList').appendChild(item)
+}
+const limparTarefa = () => {
+    const todoList = document.getElementById('todoList');
+    while (todoList.firstChild) {
+        todoList.removeChild(todoList.lastChild);
+    }
+}
+const atualizarTela = () => {
+    limparTarefa();
+    banco.forEach((item, indice) => criarItem(item.tarefa, item.status, indice));
+}
+const inserirItem = (evento) => {
+    const tecla = evento.key;
+    const texto = evento.target.value
+    if (tecla === 'Enter') {
+        banco.push({ 'tarefa': texto, 'Estudar ingles': '' });
+        atualizarTela()
+        evento.target.value = '';
+    }
+}
+const removerItem = (indice) => {
+    banco.splice(indice, 1)
+    atualizarTela()
+}
+const atualizarItem = (indice) => {
+    banco[indice].status = banco[indice].status === '' ? 'checked' : '';
+    atualizarTela();
+}
+const clickItem = (evento) => {
+    const elemento = evento.target;
+    if (elemento.type === 'button') {
+        const indice = elemento.dataset.indice;
+        removerItem(indice)
+    } else if (elemento.type === 'checkbox') {
+        const indice = elemento.dataset.indice;
+        atualizarItem(indice)
+    }
+}
+document.getElementById('newItem').addEventListener('keypress', inserirItem);
+document.getElementById('todoList').addEventListener('click', clickItem)
+atualizarTela();
